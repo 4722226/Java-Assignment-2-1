0f094403482f25af7e52a2875b71ef339b2a5353
4722226 <145943948+4722226@users.noreply.github.com>
2023-10-02 10:11:18 +0900

    Update Prog21.java

--- src/main/java/Prog21.java
+++ src/main/java/Prog21.java
@@ -1,11 +1,14 @@
 public class Prog21 {
     public static void main(String[] args) {
+        
         int x = Integer.parseInt(args[0]);
         int y = Integer.parseInt(args[1]);
         String sikiA = "x+y = ";
         String sikiB = "y/x = ";
-        System.out.println("x=" + 2 + ",y=" + 7 +"とすると、");
-        System.out.println(sikiA + x+y);
-        System.out.println(sikiB + y/x);
+        System.out.println("x=" + x + ",y=" + y +"とすると、");
+        System.out.println(sikiA + (x+y));
+        float z = x;
+        float q = y;
+        System.out.println(sikiB + q/z);
     }
 }
